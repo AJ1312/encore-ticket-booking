f0ca8e09224d68bf502e9aac51e8ec6e521d80d9
Ajitesh <13ajitesh@gmail.com>
2026-08-25 22:12:40 +0530

    chore: remove unnecessary patch and scratch files

--- apps/api/scratch.ts
+++
@@ -1,9 +0,0 @@
-import { db } from './src/db/client';
-import { seats } from './src/db/schema';
-async function run() {
-  const allSeats = await db.select().from(seats);
-  console.log('Total seats:', allSeats.length);
-  const v = allSeats.filter(s => s.venueId === '33333333-3333-4333-8333-555555555555');
-  console.log('Venue 555 seats:', v.length);
-}
-run().then(() => process.exit(0)).catch(console.error);
--- scratch.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-http.get('http://localhost:4000/api/shows/55555555-5555-4555-8555-555555555555/seats', (res) => {
-  let data = '';
-  res.on('data', chunk => data += chunk);
-  res.on('end', () => console.log('DATA:', data));
-}).on('error', (err) => console.log('ERROR:', err.message));
